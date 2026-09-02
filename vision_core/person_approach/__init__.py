4b39b847c81cd8102aecc90743e4a8b9a2b1d41e
stanisked <stanislavked@gmail.com>
2026-09-02 18:35:43 +0300

    Export person approach decision API

---
+++ vision_core/person_approach/__init__.py
@@ -0,0 +1,5 @@
+"""Dry-run stop-and-measure person-approach decisions, without actuation."""
+
+from .decision import PersonApproachDecision, PersonApproachDecisionEngine
+
+__all__ = ["PersonApproachDecision", "PersonApproachDecisionEngine"]
